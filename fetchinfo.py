d03695039232fb590d259babfe3c463cca810c42
pubuyun <wzh.zhang@qq.com>
2025-03-03 11:56:36 +0800

    update to object-based

--- fetchinfo.py
+++ fetchinfo.py
@@ -1,5 +1,5 @@
 # function encryption(){
-#         var f=$("#psid").val();
+#         var f=$("#username").val();
 #         var g=$("#passwd").val();
 #         $.ajax({
 #             url:"/user/encryption/",
@@ -8,21 +8,21 @@
 #             cache:false,
 #             async:false,
 #             dataType:"json",
-#             data:"psid="+f,
+#             data:"username="+f,
 #             success:function(a,b){
 #                 if(a["status"]=="OK"){
 #                     var c=a["salt"];
 #                     var d=c+$.md5(c+g).toUpperCase();
 #                     $("#passwd").val(d);
-#                     $("#psid_flag").val("ok");
+#                     $("#username_flag").val("ok");
 #                     $("#nosence").val(a["nosence"])
 #                 }else{
-#                     $("#psid_info_span").html(a["info"])
+#                     $("#username_info_span").html(a["info"])
 #                 }
 #             },
 #             error:function(a,b,e){
 #                 if(e){
-#                     $("#psid_info_span").html(e)
+#                     $("#username_info_span").html(e)
 #                 }
 #             }
 #         })
@@ -33,104 +33,147 @@ import time
 import hashlib
 from bs4 import BeautifulSoup
 
-def fetchinfo(psid, passwd):
+
+class cmsFetcher:
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Origin': 'https://www.alevel.com.cn',
-        'Host': 'www.alevel.com.cn',    
-        'Priority': 'u=0, i',
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Origin": "https://www.alevel.com.cn",
+        "Host": "www.alevel.com.cn",
+        "Priority": "u=0, i",
     }
-
-    session = requests.Session()
-
     mainurl = "https://www.alevel.com.cn/"
     encrypturl = "https://www.alevel.com.cn/user/encryption/"
     loginurl = "https://www.alevel.com.cn/login/"
-    scoreurl = f'https://www.alevel.com.cn/user/{psid}/assessment/list/'
-    referralurl = f'https://www.alevel.com.cn/user/{psid}/referralcomment'
 
-    session.headers.update(headers)
-    response = session.get(loginurl)
-    print('cookies', session.cookies.get_dict())
-    time.sleep(0.2)
+    def __init__(self, username, passwd):
+        self.username = username
+        self.passwd = passwd
+        self.scoreurl = f"https://www.alevel.com.cn/user/{username}/assessment/list/"
+        self.referralurl = f"https://www.alevel.com.cn/user/{username}/referralcomment"
+        self.session = requests.Session()
 
-    response = session.post(encrypturl, data=f'psid={psid}')
-    if response.status_code == 200:
-        ResponseData = response.json()
-        if ResponseData['status'] == "OK":
-            salt = ResponseData['salt']
-            PasswdEncrypted = salt + hashlib.md5((salt + passwd).encode()).hexdigest().upper()
-            Nosence = ResponseData['nosence']
-        else:
-            print("Encrypt Failed: ", ResponseData['info'])
-            return False
-    else:
-        print(f'[{response.status_code}] ', response.text)
-        return False
+    def login(self):
+        self.session.headers.update(self.headers)
+        response = self.session.get(self.loginurl)
+        time.sleep(0.2)
 
-    RequestData = {'nosence': Nosence, 'psid': psid, 'passwd': PasswdEncrypted, 'rememberpsid': 1, 'post':"登 录/Login"}
-    print(RequestData)
-    response = session.post(loginurl, data=RequestData)
-    if response.status_code == 200 :
-        if '登录' in response.text:
-            print("Login Failed: Password error or limit exceeded.")
-            return False
-        cookies = session.cookies.get_dict()
-        print('Login successfully!')
-        print("id:", psid)
-        print("Login cookies", cookies)
-    else:
-        print(f'[{response.status_code}] ', response.text)
-        
-    response = session.get(scoreurl)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        score_cells = soup.find_all(lambda tag: (
-            (tag.name == 'td' and tag.get('style') == "color:red;font-size:20px;line-height:30px;text-align: left;")
-            or
-            (tag.name == 'tr' and tag.get('bgcolor') == "#76EE00")
-    ))
-        results = {'English': [], 'Biology': [], 'Chemistry': [], 'Physics': [], 'Mathematics': [], 'Computer Science': []}
-        referrals = {'English': [], 'Biology': [], 'Chemistry': [], 'Physics': [], 'Mathematics': [], 'Computer Science': []}
-        # for every bgcolor="#76EE00" <tr> there are 7 <td>, the fifth <td> is the score, the sixth <td> is the full score
-        for score_cell in score_cells:
-            if score_cell.name == 'td':
-                currentSubject = next((subject for subject in results.keys() if subject in score_cell.text), None)
-            # if currentSubject=None, jump to next iteration
-            if currentSubject is None:
-                continue
-            if score_cell.name == 'tr':     
-                scores = score_cell.find_all('td')
-                # remove non-digit characters except dot
-                score = ''.join([char for char in scores[4].text if char.isdigit() or char == '.'])
-                full_score = ''.join([char for char in scores[5].text if char.isdigit() or char == '.'])
-                if score and full_score:
-                    results[currentSubject].append(float(score)/float(full_score))
-    else:
-        print(f'[{response.status_code}] ', response.text)
-        return False
-    
-    response = session.get(referralurl)
-    # find all <a>, text = Detail
-    soup = BeautifulSoup(response.text, 'html.parser')
-    referral_cells = soup.find_all('a', text='Detail')
-    # get the href of every <a>
-    referral_links = [mainurl+cell.get('href') for cell in referral_cells]
-    for link in referral_links:
-        response = session.get(link)
+        response = self.session.post(self.encrypturl, data=f"psid={username}")
         if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            referral = soup.find('p', text=True).string
-            subject = soup.find('strong').string
-            if subject in referrals.keys():
-                referrals[subject].append(referral)
+            ResponseData = response.json()
+            if ResponseData["status"] == "OK":
+                salt = ResponseData["salt"]
+                PasswdEncrypted = (
+                    salt + hashlib.md5((salt + passwd).encode()).hexdigest().upper()
+                )
+                Nosence = ResponseData["nosence"]
+            else:
+                print("Encrypt Failed: ", ResponseData["info"])
+                return False
         else:
-            print(f'[{response.status_code}] ', response.text)
+            print(f"[{response.status_code}] ", response.text)
             return False
-    return results, referrals
+
+        RequestData = {
+            "nosence": Nosence,
+            "psid": username,
+            "passwd": PasswdEncrypted,
+            "rememberusername": 1,
+            "post": "登 录/Login",
+        }
+        response = self.session.post(self.loginurl, data=RequestData)
+        if response.status_code == 200:
+            if "登录" in response.text:
+                print("Login Failed: Password error or limit exceeded.")
+                return False
+            cookies = self.session.cookies.get_dict()
+            print("Login successfully!")
+            print("id:", username)
+            print("Login cookies", cookies)
+            return True
+        else:
+            print(f"[{response.status_code}] ", response.text)
+            return False
+
+    def fetchScore(self):
+        response = self.session.get(self.scoreurl)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, "html.parser")
+            score_cells = soup.find_all(
+                lambda tag: (
+                    (
+                        tag.name == "td"
+                        and tag.get("style")
+                        == "color:red;font-size:20px;line-height:30px;text-align: left;"  # subject
+                    )
+                    or (tag.name == "tr" and tag.get("bgcolor") == "#76EE00")  # score
+                )
+            )
+            results = {}
+
+            # for every bgcolor="#76EE00" <tr> there are 7 <td>, the fifth <td> is the score, the sixth <td> is the full score
+            for score_cell in score_cells:
+                if score_cell.name == "td":  # the cell contains subject name
+                    currentSubject = score_cell.text.split("-")[0].strip()
+                    results[currentSubject] = []
+                if score_cell.name == "tr":  # the cell contains score
+                    scores = score_cell.find_all("td")
+                    # remove non-digit characters except dot
+                    score = "".join(
+                        [
+                            char
+                            for char in scores[4].text  # mark
+                            if char.isdigit() or char == "."
+                        ]
+                    )
+                    full_score = "".join(
+                        [
+                            char
+                            for char in scores[5].text  # outof
+                            if char.isdigit() or char == "."
+                        ]
+                    )
+                    if score and full_score:
+                        results[currentSubject].append(float(score) / float(full_score))
+        else:
+            print(f"[{response.status_code}] ", response.text)
+            return False
+        self.scores = results
+        return True
+
+    def get_referrals(self):
+        referrals = {}
+        response = self.session.get(self.referralurl)
+
+        # find all <a>, text = Detail
+        soup = BeautifulSoup(response.text, "html.parser")
+        referral_cells = soup.find_all("a", text="Detail")
+        # get the href of every <a>
+        referral_links = [self.mainurl + cell.get("href") for cell in referral_cells]
+
+        for link in referral_links:
+            response = self.session.get(link)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, "html.parser")
+                referral = soup.find("p", text=True).string
+                subject = soup.find("strong").string
+                if subject not in referrals:
+                    referrals[subject] = []
+                referrals[subject].append(referral)
+            else:
+                print(f"[{response.status_code}] ", response.text)
+                return False
+
+        self.referrals = referrals
+        return True
+
 
 if __name__ == "__main__":
-    psid = input("Input username: ")
+    username = input("Input username: ")
     passwd = input("Input password: ")
-    print(fetchinfo(psid, passwd))  
+    cms_fetcher = cmsFetcher(username, passwd)
+    if cms_fetcher.login():
+        if cms_fetcher.fetchScore():
+            print(cms_fetcher.scores)
+        if cms_fetcher.get_referrals():
+            print(cms_fetcher.referrals)
